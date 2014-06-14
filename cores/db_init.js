a14f0486ffb4897ffadd9bafe61cebbe3d28241a
matutter <matutter4@gmail.com>
2014-06-14 04:16:58 -0400

    CM getting done

--- cores/db_init.js
+++ cores/db_init.js
@@ -1,6 +1,4 @@
-// blog schema from http://www.jarrodoberto.com/_blog-assets/design-pattern-blog-erd/images/erd.png
-
-
+// blog schema mostly from http://www.jarrodoberto.com/_blog-assets/design-pattern-blog-erd/images/erd.png
 
 function blog_category() {
   this.id       = 0;
@@ -23,20 +21,45 @@ function blod_author() {
 };
 
 function blog_post() {
-  this.id = 0
-  this.title = ''
-  this.image = ''
-  this.article = ''
-  this.repoURL = ''
-  this.author_id = 0
+  this.id = 0;
+  this.title = '';
+  this.image = '';
+  this.article = '';
+  this.repoURL = '';
+  this.author_id = 0;
   this.date = new Date().toJSON();
-  this.featured = false
-  this.hidden = false
-  this.hide_comments = false
-  this.views = 0
+  this.featured = false;
+  this.hidden = false;
+  this.hide_comments = false;
+  this.views = 0;
+};
+
+function blog_tag() {
+  this.id = 0;
+  this.post_id = 0;
+  this.tag = '';
+};
+
+function blog_related() {
+  this.blog_post_id = 0
+  this.blog_related_post_ids = []
 }
 
+function blog_comment() {
+  this.id = 0;
+  this.post_id = 0;
+  this.is_reply_to_id = 0;
+  this.comment = '';
+  this.hidden = false;
+  this.date = new Date().toJSON();
+};
 
-var test = new blog_post
-
-console.log( test )
+function user() {
+  this.id = 0;
+  this.name = ''
+  this.title = ''
+  this.email = ''
+  this.websiteURL = ''
+  this.hash = ''
+  this.color = {}
+}
