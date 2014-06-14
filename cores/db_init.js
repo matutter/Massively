2d53c8fb57d2e609f1f9d6d81802975176e4f480
matutter <matutter4@gmail.com>
2014-06-14 03:00:00 -0400

    ssl n stuff

--- cores/db_init.js
+++ cores/db_init.js
@@ -3,15 +3,40 @@
 
 
 function blog_category() {
-  this.id       = 0
-  this.name     = ''
-  this.shortName= ''
-  this.enabled  = false
-  this.date     = new Date().toJSON()
+  this.id       = 0;
+  this.name     = '';
+  this.shortName= '';
+  this.hidden  = false;
+  this.date     = new Date().toJSON();
+};
+
+function blog_post_to_catagory() {
+  this.catagory_id = 0;
+  this.post_id = 0;
+};
+
+function blod_author() {
+  this.id = 0;
+  this.display_name = '';
+  this.fname = '';
+  this.lname = '';
+};
+
+function blog_post() {
+  this.id = 0
+  this.title = ''
+  this.image = ''
+  this.article = ''
+  this.repoURL = ''
+  this.author_id = 0
+  this.date = new Date().toJSON();
+  this.featured = false
+  this.hidden = false
+  this.hide_comments = false
+  this.views = 0
 }
 
 
-
-var test = new blog_category
+var test = new blog_post
 
 console.log( test )
